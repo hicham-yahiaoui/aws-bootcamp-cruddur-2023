3eff7a806a342d14097f77dd34f57a6442569196
hicham-yahiaoui <yahiaoui.hicham@yahoo.com>
2023-03-11 10:28:06 +0000

    Add cognito check on backend

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -33,6 +33,9 @@ import rollbar
 import rollbar.contrib.flask
 from flask import got_request_exception
 
+# Cognito token verifier
+from lib.cognito_jwt_token import CognitoJwtToken, extract_access_token, TokenVerifyError
+
 # Configuring Logger to Use CloudWatch
 # TODO - activate cloudwatch logs
 #LOGGER = logging.getLogger(__name__)
@@ -64,6 +67,12 @@ RequestsInstrumentor().instrument()
 # Initialize XRayMiddleware
 # TODO - X-Ray
 #XRayMiddleware(app, xray_recorder)
+# Init cognito token verifier
+cognito_jwt_token = CognitoJwtToken(
+  user_pool_id=os.getenv("AWS_COGNITO_USER_POOL_ID"), 
+  user_pool_client_id=os.getenv("AWS_COGNITO_USER_POOL_CLIENT_ID"),
+  region=os.getenv("AWS_DEFAULT_REGION")
+)
 
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
@@ -71,8 +80,8 @@ origins = [frontend, backend]
 cors = CORS(
   app, 
   resources={r"/api/*": {"origins": origins}},
-  expose_headers="location,link",
-  allow_headers="content-type,if-modified-since",
+  headers=['Content-Type', 'Authorization'], 
+  expose_headers='Authorization',
   methods="OPTIONS,GET,HEAD,POST"
 )
 
@@ -139,7 +148,6 @@ def data_messages(handle):
     return model['errors'], 422
   else:
     return model['data'], 200
-  return
 
 @app.route("/api/messages", methods=['POST','OPTIONS'])
 @cross_origin()
@@ -153,12 +161,23 @@ def data_create_message():
     return model['errors'], 422
   else:
     return model['data'], 200
-  return
 
 @app.route("/api/activities/home", methods=['GET'])
 #@xray_recorder.capture('activities_home')
 def data_home():
-  data = HomeActivities.run()
+  access_token = extract_access_token(request.headers)
+  try:
+    claims = cognito_jwt_token.verify(access_token)
+    # authenicatied request
+    print("authenicated")
+    print(claims)
+    print(claims['username'])
+    data = HomeActivities.run(cognito_user_id=claims['username'])
+  except TokenVerifyError as e:
+    # unauthenicatied request
+    print(e)
+    print("unauthenicated")
+    data = HomeActivities.run()
   return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])
@@ -183,7 +202,6 @@ def data_search():
     return model['errors'], 422
   else:
     return model['data'], 200
-  return
 
 @app.route("/api/activities", methods=['POST','OPTIONS'])
 @cross_origin()
@@ -196,7 +214,6 @@ def data_activities():
     return model['errors'], 422
   else:
     return model['data'], 200
-  return
 
 @app.route("/api/activities/<string:activity_uuid>", methods=['GET'])
 #@xray_recorder.capture('activities_show')
@@ -214,7 +231,6 @@ def data_activities_reply(activity_uuid):
     return model['errors'], 422
   else:
     return model['data'], 200
-  return
 
 if __name__ == "__main__":
   app.run(debug=True)
